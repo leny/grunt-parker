338a130f5236142399646db89f02c29816affdd3
leny <info@flatland.be>
2014-09-14 18:18:48 +0200

    Base task code

---
+++ src/parker.coffee
@@ -0,0 +1,116 @@
+###
+ * grunt-todo
+ * https://github.com/Leny/grunt-todo
+ *
+ * Copyright (c) 2013 Leny
+ * Licensed under the MIT license.
+###
+
+"use strict"
+
+chalk = require "chalk"
+Parker = require "parker"
+
+module.exports = ( grunt ) ->
+
+    grunt.registerMultiTask "parker", "Stylesheet analysis", ->
+        oOptions = @options
+            metrics: "all"
+            file: no
+            title: no
+            colophon: no
+            usePackage: no
+        aLogFileLines = []
+        sDefaultTitle = "Grunt Parker Report"
+
+        try
+            aMetrics = require "../node_modules/parker/metrics/#{ oOptions.metrics }"
+        catch oError
+            grunt.log.writeln ""
+            grunt.log.writeln chalk.yellow.bold( "Oops:" ), "No file for metric #{ chalk.cyan( oOptions.metrics ) } found. Disabling #{ chalk.green( 'metrics' ) } option."
+            aMetrics = require "../node_modules/parker/metrics/all"
+
+        parker = new Parker aMetrics
+
+        oParsedMetrics = {}
+        for oMetric in aMetrics
+            oParsedMetrics[ oMetric.id ] = oMetric
+
+        if oOptions.usePackage
+            try
+                oProjectPackage = grunt.file.readJSON "#{ process.cwd() }/package.json"
+            catch oError
+                grunt.log.writeln ""
+                grunt.log.writeln chalk.yellow.bold( "Oops:" ), "No #{ chalk.cyan( 'package.json' ) } file found. Disabling #{ chalk.green( 'usePackage' ) } option."
+                oOptions.usePackage = no
+
+        if oOptions.file
+            if sTitle = ( oOptions.title or ( if oOptions.usePackage and oProjectPackage.name then oProjectPackage.name else no ) or sDefaultTitle )
+                if oOptions.usePackage
+                    if sHomePage = oProjectPackage.homepage
+                        aLogFileLines.push "# [#{ sTitle }]( #{ sHomePage } )"
+                    else
+                        aLogFileLines.push "# #{ sTitle }"
+                    aLogFileLines.push ""
+                    if sVersion = oProjectPackage.version
+                        aLogFileLines.push "**Version:** `#{ sVersion }`"
+                        aLogFileLines.push ""
+                    if sDescription = oProjectPackage.description
+                        aLogFileLines.push "> #{ sDescription }"
+                        aLogFileLines.push ""
+                        aLogFileLines.push "* * *"
+                        aLogFileLines.push ""
+                else
+                    aLogFileLines.push "# #{ sTitle }"
+                    aLogFileLines.push ""
+                aLogFileLines.push "## Parker Report" unless sTitle is sDefaultTitle
+            else
+                aLogFileLines.push "# #{ sDefaultTitle }"
+            aLogFileLines.push ""
+
+        @filesSrc
+            .filter ( sFilePath ) ->
+                grunt.file.exists( sFilePath ) and grunt.file.isFile( sFilePath )
+            .forEach ( sFilePath ) ->
+                aResults = []
+                aFileResults = []
+                oParkerMetrics = parker.run grunt.file.read sFilePath
+
+                if oParkerMetrics
+                    for sMetric, mValue of oParkerMetrics
+                        aResults.push [
+                            oParsedMetrics[ sMetric ].name
+                            mValue
+                        ]
+
+                    if aResults.length
+                        grunt.log.writeln()
+                        grunt.log.writeln chalk.underline sFilePath
+                        grunt.log.writeln()
+                        for aResult in aResults
+                            sValue = switch grunt.util.kindOf aResult[ 1 ]
+                                when "array"
+                                    grunt.log.writeln chalk.cyan( "#{ aResult[ 0 ] }:" )
+                                    grunt.log.writeln "\t#{ sResult }" for sResult in aResult[ 1 ]
+                                    aFileResults.push "- **#{ aResult[ 0 ] }:**"
+                                    for sResult in aResult[ 1 ]
+                                        if sResult.substring( 0, 1 ) is "#"
+                                            sResult = "`#{ sResult }`"
+                                        aFileResults.push "\t- #{ sResult }"
+                                when "number"
+                                    grunt.log.writeln chalk.cyan( "#{ aResult[ 0 ] }:" ), chalk.yellow aResult[ 1 ]
+                                    aFileResults.push "- **#{ aResult[ 0 ] }:** #{ aResult[ 1 ] }"
+                                else
+                                    grunt.log.writeln chalk.cyan( "#{ aResult[ 0 ] }:" ), aResult[ 1 ]
+                                    aFileResults.push "- **#{ aResult[ 0 ] }:** #{ aResult[ 1 ] }"
+
+                if oOptions.file and aFileResults.length
+                    aLogFileLines.push "### #{ sFilePath }"
+                    aLogFileLines.push ""
+                    aLogFileLines = aLogFileLines.concat aFileResults
+                    aLogFileLines.push ""
+
+        if oOptions.file
+            grunt.file.write oOptions.file, aLogFileLines.join "\n"
+            grunt.log.writeln()
+            grunt.log.writeln "Logged in #{ chalk.yellow( oOptions.file ) }"
---
+++ tasks/parker.js
@@ -0,0 +1,147 @@
+
+/*
+ * grunt-todo
+ * https://github.com/Leny/grunt-todo
+ *
+ * Copyright (c) 2013 Leny
+ * Licensed under the MIT license.
+ */
+"use strict";
+var Parker, chalk;
+
+chalk = require("chalk");
+
+Parker = require("parker");
+
+module.exports = function(grunt) {
+  return grunt.registerMultiTask("parker", "Stylesheet analysis", function() {
+    var aLogFileLines, aMetrics, oError, oMetric, oOptions, oParsedMetrics, oProjectPackage, parker, sDefaultTitle, sDescription, sHomePage, sTitle, sVersion, _i, _len;
+    oOptions = this.options({
+      metrics: "all",
+      file: false,
+      title: false,
+      colophon: false,
+      usePackage: false
+    });
+    aLogFileLines = [];
+    sDefaultTitle = "Grunt Parker Report";
+    try {
+      aMetrics = require("../node_modules/parker/metrics/" + oOptions.metrics);
+    } catch (_error) {
+      oError = _error;
+      grunt.log.writeln("");
+      grunt.log.writeln(chalk.yellow.bold("Oops:"), "No file for metric " + (chalk.cyan(oOptions.metrics)) + " found. Disabling " + (chalk.green('metrics')) + " option.");
+      aMetrics = require("../node_modules/parker/metrics/all");
+    }
+    parker = new Parker(aMetrics);
+    oParsedMetrics = {};
+    for (_i = 0, _len = aMetrics.length; _i < _len; _i++) {
+      oMetric = aMetrics[_i];
+      oParsedMetrics[oMetric.id] = oMetric;
+    }
+    if (oOptions.usePackage) {
+      try {
+        oProjectPackage = grunt.file.readJSON("" + (process.cwd()) + "/package.json");
+      } catch (_error) {
+        oError = _error;
+        grunt.log.writeln("");
+        grunt.log.writeln(chalk.yellow.bold("Oops:"), "No " + (chalk.cyan('package.json')) + " file found. Disabling " + (chalk.green('usePackage')) + " option.");
+        oOptions.usePackage = false;
+      }
+    }
+    if (oOptions.file) {
+      if (sTitle = oOptions.title || (oOptions.usePackage && oProjectPackage.name ? oProjectPackage.name : false) || sDefaultTitle) {
+        if (oOptions.usePackage) {
+          if (sHomePage = oProjectPackage.homepage) {
+            aLogFileLines.push("# [" + sTitle + "]( " + sHomePage + " )");
+          } else {
+            aLogFileLines.push("# " + sTitle);
+          }
+          aLogFileLines.push("");
+          if (sVersion = oProjectPackage.version) {
+            aLogFileLines.push("**Version:** `" + sVersion + "`");
+            aLogFileLines.push("");
+          }
+          if (sDescription = oProjectPackage.description) {
+            aLogFileLines.push("> " + sDescription);
+            aLogFileLines.push("");
+            aLogFileLines.push("* * *");
+            aLogFileLines.push("");
+          }
+        } else {
+          aLogFileLines.push("# " + sTitle);
+          aLogFileLines.push("");
+        }
+        if (sTitle !== sDefaultTitle) {
+          aLogFileLines.push("## Parker Report");
+        }
+      } else {
+        aLogFileLines.push("# " + sDefaultTitle);
+      }
+      aLogFileLines.push("");
+    }
+    this.filesSrc.filter(function(sFilePath) {
+      return grunt.file.exists(sFilePath) && grunt.file.isFile(sFilePath);
+    }).forEach(function(sFilePath) {
+      var aFileResults, aResult, aResults, mValue, oParkerMetrics, sMetric, sResult, sValue, _j, _len1;
+      aResults = [];
+      aFileResults = [];
+      oParkerMetrics = parker.run(grunt.file.read(sFilePath));
+      if (oParkerMetrics) {
+        for (sMetric in oParkerMetrics) {
+          mValue = oParkerMetrics[sMetric];
+          aResults.push([oParsedMetrics[sMetric].name, mValue]);
+        }
+        if (aResults.length) {
+          grunt.log.writeln();
+          grunt.log.writeln(chalk.underline(sFilePath));
+          grunt.log.writeln();
+          for (_j = 0, _len1 = aResults.length; _j < _len1; _j++) {
+            aResult = aResults[_j];
+            sValue = (function() {
+              var _k, _l, _len2, _len3, _ref, _ref1, _results;
+              switch (grunt.util.kindOf(aResult[1])) {
+                case "array":
+                  grunt.log.writeln(chalk.cyan("" + aResult[0] + ":"));
+                  _ref = aResult[1];
+                  for (_k = 0, _len2 = _ref.length; _k < _len2; _k++) {
+                    sResult = _ref[_k];
+                    grunt.log.writeln("\t" + sResult);
+                  }
+                  aFileResults.push("- **" + aResult[0] + ":**");
+                  _ref1 = aResult[1];
+                  _results = [];
+                  for (_l = 0, _len3 = _ref1.length; _l < _len3; _l++) {
+                    sResult = _ref1[_l];
+                    if (sResult.substring(0, 1) === "#") {
+                      sResult = "`" + sResult + "`";
+                    }
+                    _results.push(aFileResults.push("\t- " + sResult));
+                  }
+                  return _results;
+                  break;
+                case "number":
+                  grunt.log.writeln(chalk.cyan("" + aResult[0] + ":"), chalk.yellow(aResult[1]));
+                  return aFileResults.push("- **" + aResult[0] + ":** " + aResult[1]);
+                default:
+                  grunt.log.writeln(chalk.cyan("" + aResult[0] + ":"), aResult[1]);
+                  return aFileResults.push("- **" + aResult[0] + ":** " + aResult[1]);
+              }
+            })();
+          }
+        }
+      }
+      if (oOptions.file && aFileResults.length) {
+        aLogFileLines.push("### " + sFilePath);
+        aLogFileLines.push("");
+        aLogFileLines = aLogFileLines.concat(aFileResults);
+        return aLogFileLines.push("");
+      }
+    });
+    if (oOptions.file) {
+      grunt.file.write(oOptions.file, aLogFileLines.join("\n"));
+      grunt.log.writeln();
+      return grunt.log.writeln("Logged in " + (chalk.yellow(oOptions.file)));
+    }
+  });
+};
